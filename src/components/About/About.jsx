5371705d47a131c197b95921d7f3e7091ab1466d
KhomotsoTibane <jledoza@gmail.com>
2022-09-21 20:27:41 +0200

    image animation

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,8 +4,8 @@ import About1 from "../../assets/about1.png"
 import About2 from "../../assets/about2.jpg"
 import About3 from "../../assets/about3.jpg"
 import Hr from "../../assets/hr.png"
-import { AiOutlineArrowRight } from "react-icons/ai"
-import { motion } from 'framer-motion'
+import { AiOutlineArrowRight, AiOutlineRotateLeft } from "react-icons/ai"
+import { motion, transform } from 'framer-motion'
 
 const About = () => {
 
@@ -13,8 +13,8 @@ const About = () => {
     <section id='about'>
       <div className="about">
         <motion.div
-          initial={{x: 0 }}
-          whileInView={{x: [10,0,10,0] }}
+          initial={{ x: 0 }}
+          whileInView={{ x: [10, 0, 10, 0] }}
           transition={{ type: 'tween', duration: 3, ease: "linear" }}
         >
           <img src={Hr} alt="" width={130} height={20} />
@@ -26,7 +26,11 @@ const About = () => {
             <span>Infants as young as 6 months old can benefit from group swimming lessons.</span>
             <a href="#schedule">view schedule <AiOutlineArrowRight /></a>
             <div>
-              <img src={About1} alt="" />
+              <motion.img
+                initial={{ x: 0 , }}
+                whileInView={{ x: -70, y:0 }}
+                transition={{ type: 'tween', duration: 1, ease: "linear" }}
+                src={About1} alt="" />
             </div>
           </div>
           <div className="card">
@@ -34,7 +38,11 @@ const About = () => {
             <span>We taught thousands of children how to love the water, it's time for you to jump in.</span>
             <a href="#schedule">view schedule <AiOutlineArrowRight /> </a>
             <div>
-              <img src={About2} alt="" />
+              <motion.img
+                initial={{ x: 0 }}
+                whileInView={{ x: -70 }}
+                transition={{ type: 'tween', duration: 1, ease: "linear" }}
+                src={About2} alt="" />
             </div>
           </div>
           <div className="card">
@@ -42,7 +50,11 @@ const About = () => {
             <span>Private lessons are 30 minutes long and pair one swimmer with one instructor.</span>
             <a href="">view more <AiOutlineArrowRight /></a>
             <div>
-              <img src={About3} alt="" />
+              <motion.img
+                initial={{ x: 0 }}
+                whileInView={{ x: -70, y:25 }}
+                transition={{ type: 'tween', duration: 1, ease: "linear" }}
+                src={About3} alt="" />
             </div>
           </div>
         </div>
